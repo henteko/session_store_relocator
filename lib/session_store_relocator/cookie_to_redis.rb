e74f67b4ad7e1a964fdbe031d6d017b857cb80d5
henteko <henteko07@gmail.com>
2016-07-08 03:29:23 +0900

    test13

--- lib/session_store_relocator/cookie_to_redis.rb
+++ lib/session_store_relocator/cookie_to_redis.rb
@@ -4,7 +4,7 @@ module SessionStoreRelocator
   class CookieToRedis < RedisSessionStore
     def initialize(app, options = {})
       super(app, options[:redis_session_store])
-      @cookie_store = ActionDispatch::Session::CookieStore.new(app, options[:cookie_store])
+      @cookie_store = CookieStore.new(app, options[:cookie_store])
     end
 
     def destroy_session(*args)
@@ -15,15 +15,26 @@ module SessionStoreRelocator
 
     def get_session(*args)
       env = args.first
-      session = @cookie_store.send(:load_session, env)
-      data = session.last
-      session_id = data.delete('session_id')
-      unless data.empty?
-        @cookie_store.send(:destroy_session, *[env, session_id, {}])
-        return session
+      session_id = args.last
+      session = session_class.new(@cookie_store, env)
+      unless session.empty?
+        session.destroy
+        return [session_id, session.to_hash]
       end
 
       super
+    end
+  end
+
+  class CookieStore < ActionDispatch::Session::CookieStore
+    def initialize(app, options = {})
+      super(app, options)
+    end
+
+    def destroy_session(*args)
+      env = args.first
+      session_id = args.second
+      set_cookie(env, session_id, {})
     end
   end
 end
